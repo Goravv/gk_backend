43acebdfd83dc6e29f5ca22a7e7b00e674d87a65
Gaurav Kumar <gaurav3108billa@gmail.com>
2025-07-08 13:01:42 +0530

    reactnative issue fix 2

--- export/settings.py
+++ export/settings.py
@@ -79,7 +79,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # CORS & CSRF
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://gk-backend-c2ih.onrender.com",
     "http://localhost:3000", "http://127.0.0.1:3000",
@@ -113,3 +113,17 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'},
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
