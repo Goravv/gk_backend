ecb83ced00f6db17eb3d15c6cb9a012d78846596
Gaurav Kumar <gaurav3108billa@gmail.com>
2025-07-26 18:40:32 +0530

    self user

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets, permissions
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [permissions.IsAuthenticated]  # or [permissions.AllowAny] if public access
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
