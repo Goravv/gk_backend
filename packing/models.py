88117e9ec26078f35b3d9d0293d6de65c9bcd0d1
Gaurav Kumar <gaurav3108billa@gmail.com>
2025-08-20 16:07:44 +0530

    packing list2

--- packing/models.py
+++ packing/models.py
@@ -16,7 +16,7 @@ class Packing(models.Model):
         ]
 
     def __str__(self):
-        return f"Packing #{self.packing_no}"
+        return f"Packing #{self.part_no}"
 
 
 class Stock(models.Model):
